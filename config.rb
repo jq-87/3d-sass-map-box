f939cb70f33fc52d48b2b03cfe0231528b0ecd64
neco <neco@elneco.biz>
2014-10-05 22:04:45 -0400

    initial commit

---
+++ config.rb
@@ -0,0 +1,8 @@
+# http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+# images_dir = 'assets/images'
+# javascripts_dir = 'assets/javascripts'
+# relative_assets = true
+line_comments = false
+output_style = :expanded
